1f7317c4236b5b67c32d8f671daaa3cef4a5485c
petrol95 <petrol195@yandex.ru>
2019-03-25 18:30:05 +0300

    added queries

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -19,6 +19,22 @@ public class Book {
         this.title = title;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     @Override
     public String toString() {
         return "Book [id = " + id + ", title='" + title + "']";
--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -2,6 +2,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 public class MainClass {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
@@ -11,12 +13,51 @@ public class MainClass {
 
         Session session = null;
         try {
-            session = factory.getCurrentSession();
 
-            Book tmpBook = new Book("Harry Potter");
+            // Добавить книгу
+//            session = factory.getCurrentSession();
+//            Book tmpBook = new Book("Harry Potter");
+//            session.beginTransaction();
+//            session.save(tmpBook);
+//            session.getTransaction().commit();
+
+            // Получить книгу
+//            session = factory.getCurrentSession();
+//            session.beginTransaction();
+//            Book harryPotter2 = session.get(Book.class, 2);
+//            session.getTransaction().commit();
+//            System.out.println(harryPotter2);
+
+            // Обновить книгу
+//            session = factory.getCurrentSession();
+//            session.beginTransaction();
+//            Book harryPotter3 = session.get(Book.class, 3);
+//            harryPotter3.setTitle("Harry Potter 3 Updated");
+//            session.getTransaction().commit();
+//            System.out.println(harryPotter3);
+
+            // Удалить книгу
+//            session = factory.getCurrentSession();
+//            session.beginTransaction();
+//            Book trashBook = session.get(Book.class, 4);
+//            session.delete(trashBook);
+//            session.getTransaction().commit();
+
+            // Получить список всех книг
+            session = factory.getCurrentSession();
             session.beginTransaction();
-            session.save(tmpBook);
+            List<Book> allBooks = session.createQuery("FROM Book").getResultList();
+            System.out.println(allBooks);
+            // from Book b where b.title='Harry Potter' OR b.authorName='Rowling'
+            //     OR b.title LIKE 'Harry%'
             session.getTransaction().commit();
+
+            // Массовое обновление
+//            session = factory.getCurrentSession();
+//            session.beginTransaction();
+//            session.createQuery("update Book set title='A';").executeUpdate();
+//            session.createQuery("delete from Book where id=50").executeUpdate();
+//            session.getTransaction().commit();
 
         } finally {
             factory.close();
